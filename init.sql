fbb279d5e273dea744db11d8e54dcfa83aeefaef
Arthur Vincent Simon <developarvin@gmail.com>
2020-12-18 11:18:17 +0800

    More updates to SQL

--- init.sql
+++ init.sql
@@ -1,11 +1,8 @@
-SELECT DATABASE "postgres"
-
-# Creating Tables
-START TRANSACTION;
+-- Creating Tables
 DROP TABLE IF EXISTS players;
 CREATE TABLE players
 (
-    id   serial not null
+    id serial not null
         constraint players_pk
             primary key,
     name text
@@ -14,43 +11,33 @@ CREATE TABLE players
 DROP TABLE IF EXISTS scores;
 CREATE TABLE scores
 (
-    id           serial not null
+    id serial not null
         constraint table_name_pk
             primary key,
-    score        integer,
-    player_id    integer
+    score integer,
+    player_id integer
         constraint table_name_players_id_fk
             references players,
     date_created timestamp
 );
-END TRANSACTION;
 
-# Insert data
-START TRANSACTION;
-INSERT INTO players(name)
-VALUES ("John")
-RETURNING id  AS p_id;
+-- Insert data
+INSERT INTO players (name)
+VALUES ('John')
+RETURNING id as john_id;
 
-INSERT INTO scores(score, player_id, date_created)
-VALUES (111, p_id,1607751459)
+INSERT
+INTO scores(score, player_id, date_created)
+VALUES (111, 1, now()),
+       (122, 1, now()),
+       (133, 1, now());
 
-INSERT INTO scores(score, player_id, date_created)
-VALUES (122, p_id,1607837857)
+INSERT INTO players (name)
+VALUES ('Jane')
+RETURNING id as jane_id;
 
-INSERT INTO scores(score, player_id, date_created)
-VALUES (133, p_id,1607924257)
-
-INSERT INTO players(name)
-VALUES ("Jane")
-RETURNING id AS p_id;
-
-INSERT INTO scores(score, player_id, date_created)
-VALUES (211, p_id,1607751459)
-
-INSERT INTO scores(score, player_id, date_created)
-VALUES (222, p_id,1607837857)
-
-INSERT INTO scores(score, player_id, date_created)
-VALUES (233, p_id,1607924257)
-END TRANSACTION;
-
+INSERT
+INTO scores(score, player_id, date_created)
+VALUES (211, 2, now()),
+       (222, 2, now()),
+       (233, 2, now());
